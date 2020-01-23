415bfc9cdaa91c34fca069d2ca2e8dbadc8f2e5b
Rafal Kopacewicz <rkopace@us.ibm.com>
2020-01-22 23:16:17 -0500

    Test comment and updated .gitignore file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Test comment
+		
 		System.out.println("Witaj swiecie !!!");
 	}
 
